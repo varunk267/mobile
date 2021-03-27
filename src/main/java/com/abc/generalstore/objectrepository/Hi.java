f7326be6af86d0fac6b5be1a1a3d693e4a1b77e1
varunk267 <varun.varu2671992@gmail.com>
2021-03-28 02:08:33 +0530

    message

---
+++ src/main/java/com/abc/generalstore/objectrepository/Hi.java
@@ -0,0 +1,10 @@
+package com.abc.generalstore.objectrepository;
+
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.MobileElement;
+
+public class Hi {
+@FindBy(xpath="")
+private MobileElement a;
+}
